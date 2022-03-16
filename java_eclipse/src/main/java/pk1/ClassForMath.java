ccf1f11dcb59c40493c547e36c3655db0ed0a6be
Inna Yankelevich <inna_yan20@inbox.ru>
2022-03-16 18:22:05 +0300

    aaa

---
+++ java_eclipse/src/main/java/pk1/ClassForMath.java
@@ -0,0 +1,50 @@
+package main.java.pk1;
+
+public class ClassForMath {
+
+    public int multiply(int x, int y) {
+            return x * y;
+        }
+   //dddddddddddddddddd 
+    
+    public int summa (int x, int y) {
+        return (x+ y);
+    }
+    public int difference (int x, int y) {
+        return (x - y);   
+    }
+    public int division (int x, int y) {
+            return (x / y); 
+            ///dfsdfsdffs
+    }
+    public int division2 (int x, int y) {
+        return (x / y); 
+        ///dfsdfsdffs
+}
+    public int division3 (int x, int y) {
+        return (x / y); 
+        ///dfsdfsdffs
+}
+    public int division4 (int x, int y) {
+        System.out.println("Bye");
+        System.out.println("Tesä,öorü");
+         return (x / y); 
+        ///dfsdfsdffs
+}
+   
+    public int double_summa (int x, int y) {
+        return (2*(x+y));
+    }
+    public int triple_summa (int x, int y) {
+        return (3*(x+y));
+    }
+    public int summa_minus (int x, int y) {
+        return ((x+y)/2);
+    }
+    
+            
+    public int multiply2(int x, int y) {
+                return x * y;
+            }
+////fgdfgdfgdfgdgdfgdg.....
+}
---
+++ java_eclipse/src/test/java/pk1a/TestClassForMath3a.java
@@ -0,0 +1,77 @@
+package test.java.pk1;
+
+import main.java.pk1.ClassForMath;
+import junit.framework.*;
+//fff\rttttttttttttttttt
+////yyyyy
+//tg
+
+
+
+public class TestClassForMath3a extends TestCase {
+
+        public void testMultiply() {
+            ClassForMath tester = new ClassForMath();
+            System.out.println("1");
+            System.out.print("2");
+            System.out.print("hello");
+            System.err.print("hello again");
+               Assert.assertEquals("Result", 30, tester.multiply(6, 5));
+
+        }
+        public void testMultiply2() {
+            ClassForMath tester = new ClassForMath();
+            System.out.print("2");
+            System.out.print("hello");
+            System.err.print("hello again");
+               Assert.assertEquals("Result", 30, tester.multiply2(6, 5));
+       }
+////t
+
+public void testSumma() {
+	
+    ClassForMath tester = new ClassForMath();
+    System.out.println("3");
+    
+       Assert.assertEquals("Result", 18, tester.summa(12, 5));
+//dddddd
+}
+public void testDifference() {
+    ClassForMath tester = new ClassForMath();
+    System.out.println("4");
+       Assert.assertEquals("Result", 9, tester.difference(12, 5));
+
+}
+public void testDifference2() {
+//sdfsfdfsdfdfsdf
+	//sdfsdfsdfsdfsdf
+	//dfkdkfjskfjsldkfjslkdjflskjflksdjfsf
+    ClassForMath tester = new ClassForMath();
+       Assert.assertEquals("Result", 7, tester.difference(12, 5));
+
+}
+public void testDiffer1234567890123456789012345678901234567890v12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890ence1() {
+    ClassForMath tester = new ClassForMath();
+       Assert.assertEquals("Result", 8, tester.difference(12, 5));
+
+}
+public void testDivision() {
+    ClassForMath tester = new ClassForMath();
+       Assert.assertEquals("Result", 8, tester.division(8,1));}
+
+public void testDoubleSumma() {
+	//dfdfdfsfsdfsfdsfsf
+	//dfsfdfsdfdfsdfsff
+    ClassForMath tester = new ClassForMath();
+       Assert.assertEquals("Result", 35, tester.double_summa(12, 5));
+       //zzxzxzxzxzxzxzxzxzxzx
+       
+}
+public void testTripleSumma()
+{ClassForMath test1= new ClassForMath();
+Assert.assertEquals("Result", 11, test1.triple_summa(2,2) );
+	}
+
+
+}
+
